21a86f8ba9d6330a8d8f63f0460cc3ce38d29768
aynursamur <73234870+aynursamur@users.noreply.github.com>
2023-01-28 10:21:22 +0000

    Deploying to gh-pages from @ aynursamur/aynursamur.github.io@62a84b7bef76a4ca06109e66d674ec0a0a962ece 🚀

--- README.md
+++
@@ -1,1 +0,0 @@
-# aynursamur.github.io
---
+++ _next/static/rJQjxjei5pUfGcbSXpGm9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/288-a8c95a1b4efa422b.js","static/chunks/pages/index-437180e50956de89.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
